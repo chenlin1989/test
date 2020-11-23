17fb6f0abcc87a176dd9a0ed48f4463c57c07fc4
chenlin1989 <chenlin1989@126.com>
2020-11-24 00:10:29 +0800

    first add

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+
+
+    public static void main(String[] args) {
+
+        int a;
+        addfef;
+        int Systaftem = 1;
+        System.out.println("args = " + java.util.Arrays.deepToString(args));
+    }
+}
